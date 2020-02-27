d21dbf4201c1999c9fbd01290f18f749384324cf
nghia <nghiabt@mageplaza.com>
2020-02-27 15:07:30 +0700

    graphQl done

--- Model/Resolver/Filter/Query/Filter.php
+++ Model/Resolver/Filter/Query/Filter.php
@@ -25,8 +25,10 @@ namespace Mageplaza\ReportsGraphQl\Model\Resolver\Filter\Query;
 
 use Magento\Framework\App\RequestInterface;
 use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Framework\GraphQl\Exception\GraphQlInputException;
 use Magento\Framework\GraphQl\Query\Resolver\Argument\SearchCriteria\Builder;
 use Mageplaza\Reports\Api\CardManagementInterface;
+use Mageplaza\Reports\Helper\Data;
 use Mageplaza\ReportsGraphQl\Model\Resolver\Filter\SearchResult;
 use Mageplaza\ReportsGraphQl\Model\Resolver\Filter\SearchResultFactory;
 
@@ -53,60 +55,62 @@ class Filter
      * @var RequestInterface
      */
     private $request;
+    /**
+     * @var Data
+     */
+    private $helperData;
 
     /**
      * Filter constructor.
      *
      * @param Builder $searchCriteriaBuilder
      * @param SearchResultFactory $searchResultFactory
+     * @param RequestInterface $request
      * @param CardManagementInterface $cardManagement
+     * @param Data $helperData
      */
     public function __construct(
         Builder $searchCriteriaBuilder,
         SearchResultFactory $searchResultFactory,
         RequestInterface $request,
-        CardManagementInterface $cardManagement
+        CardManagementInterface $cardManagement,
+        Data $helperData
     ) {
         $this->searchCriteriaBuilder = $searchCriteriaBuilder;
-        $this->searchResultFactory = $searchResultFactory;
-        $this->cardManagement = $cardManagement;
-        $this->request = $request;
+        $this->searchResultFactory   = $searchResultFactory;
+        $this->cardManagement        = $cardManagement;
+        $this->request               = $request;
+        $this->helperData            = $helperData;
     }
 
     /**
-     * @param array $args
-     * @param string $type
+     * @param $args
+     * @param $name
      *
      * @return SearchResult
+     * @throws GraphQlInputException
      */
-//    public function getResult($args, $type): SearchResult
-//    {
-//        $searchCriteria = $this->searchCriteriaBuilder->build($type, $args);
-//        $searchCriteria->setCurrentPage($args['currentPage']);
-//        $searchCriteria->setPageSize($args['pageSize']);
-//
-//        switch ($type) {
-//            case 'mpGiftCode':
-//                $list = $this->giftCodeManagement->getList($searchCriteria);
-//                break;
-//            case 'mpGiftPool':
-//                $list = $this->giftPoolManagement->getList($searchCriteria);
-//                break;
-//            case 'mpGiftTemplate':
-//            default:
-//                $list = $this->giftTemplateManagement->getList($searchCriteria);
-//                break;
-//        }
-//
-//        $listArray = [];
-//        /** @var GiftCardModel|PoolModel|TemplateModel $item */
-//        foreach ($list->getItems() as $item) {
-//            $listArray[$item->getId()]          = $item->getData();
-//            $listArray[$item->getId()]['model'] = $item;
-//        }
-//
-//        return $this->searchResultFactory->create($list->getTotalCount(), $listArray);
-//    }
+    public function getResult($args, $name): SearchResult
+    {
+        $searchCriteria = $this->searchCriteriaBuilder->build($name, $args);
+        $searchCriteria->setCurrentPage($args['currentPage']);
+        $searchCriteria->setPageSize($args['pageSize']);
+
+        if (!class_exists('\Mageplaza\ReportsPro\Model\Api\DetailManagement')) {
+            throw new GraphQlInputException(__('Module Mageplaza_ReportsPro does not exits'));
+        }
+
+        $detailManagement = $this->helperData->createObject('\Mageplaza\ReportsPro\Model\Api\DetailManagement');
+        $list             = $detailManagement->get($searchCriteria, $name);
+        $listArray        = [];
+
+        foreach ($list->getItems() as $item) {
+            $listArray[$item->getId()]          = $item->getData();
+            $listArray[$item->getId()]['model'] = $item;
+        }
+
+        return $this->searchResultFactory->create($list->getTotalCount(), $listArray);
+    }
 
     /**
      * @param $name
@@ -135,6 +139,7 @@ class Filter
             case 'salesByHoursChart':
             case 'salesByWeekdayChart':
                 $card = $this->cardManagement->get($name);
+
                 return $card->getData();
             case 'bestsellers':
             case 'customers':
